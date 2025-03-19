d77c154928266f905b77aa44c78d2aab39c6f066
Testeriusz <testeriusztester@gmail.com>
2025-03-19 11:48:25 +0100

    name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 }
 
 variable "clusterName" {
-  description = "Name of the EKS cluster"
+  description = "name of the EKS cluster"
   type        = string
   default     = "vprofile-eks"
 }
